b74415bdd1a97f9a8a6f915102df94c9ce9def70
Nando Machado <icenando@gmail.com>
2022-12-04 08:23:32 +0000

    day 4 part 2

--- 4/cleaning.py
+++ 4/cleaning.py
@@ -15,7 +15,19 @@ def part_1():
 
 
 def part_2():
-    return
+    with (open("4/input.txt") as f):
+        overlapsFullyTotal = 0
+        lines = f.readlines()
+        for line in lines:
+            areas = line.split(",")
+            elf_1, elf_2 = areas[0].split("-"), areas[1].replace("\n", "").split("-")
+            elf_1, elf_2 = [int(area) for area in elf_1], [int(area) for area in elf_2]
+            if (max(elf_1) >= min(elf_2) and min(elf_1) <= max(elf_2)) or (
+                min(elf_2) >= max(elf_1) and max(elf_2) <= min(elf_1)
+            ):
+                overlapsFullyTotal += 1
+
+    print(overlapsFullyTotal)
 
 
 part_1()
